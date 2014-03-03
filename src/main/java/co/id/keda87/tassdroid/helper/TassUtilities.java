c7d5f3583f7dfc6bc6b74f4ce3f55c73e35c756d
Keda87 <keda_87@yahoo.com>
2014-03-03 13:18:58 +0700

    tambahin API builder

--- src/main/java/co/id/keda87/tassdroid/helper/TassUtilities.java
+++ src/main/java/co/id/keda87/tassdroid/helper/TassUtilities.java
@@ -2,6 +2,7 @@ package co.id.keda87.tassdroid.helper;
 
 import android.annotation.TargetApi;
 import android.os.Build;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,8 +14,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,31 +23,29 @@ import java.util.logging.Logger;
  */
 public class TassUtilities {
 
+    static final String BASE_API_URL = "http://tass.telkomuniversity.ac.id/telkomuniversity.php/api?key=";
+
     /**
      * Fungsi untuk mengubah String menjadi hash dengan metode md5
      *
-     * @param token
+     * @param password : plain password mahasiswa di student portal
      * @return md5 hash result
      */
-    public static String md5(String token) {
+    public static String md5(String password) {
         StringBuffer sbuf = null;
         MessageDigest md = null;
 
         try {
             md = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
-            System.out.println(e.getMessage());
+            Log.e("KESALAHAN", e.getMessage());
         }
 
-        md.update(token.getBytes());
+        md.update(password.getBytes());
         byte bytePwd[] = md.digest();
 
         if (sbuf == null) {
             sbuf = new StringBuffer();
-            for (int i = 0; i < bytePwd.length; i++) {
-                sbuf.append(Integer.toString((bytePwd[i] & 0xff) + 0x100, 16).substring(1));
-            }
-        } else {
             for (int i = 0; i < bytePwd.length; i++) {
                 sbuf.append(Integer.toString((bytePwd[i] & 0xff) + 0x100, 16).substring(1));
             }
@@ -61,7 +58,7 @@ public class TassUtilities {
      *
      * @return angka random
      */
-    public static int randomGenerator() {
+    private static int randomGenerator() {
         int random = (int) (Math.random() * 10);
         int a = random == 0 ? random + 1 : random;
         return a;
@@ -70,10 +67,10 @@ public class TassUtilities {
     /**
      * Fungsi untuk membuat key token
      *
-     * @param rand angka random yang digenerate oleh sistem
+     * @param rand : angka random yang digenerate oleh sistem
      * @return token dengan bentuk md5 hash
      */
-    public static String generateToken(int rand) {
+    private static String generateToken(int rand) {
         int a = rand;
         String hasil = "";
         String c = "";
@@ -121,20 +118,55 @@ public class TassUtilities {
             }
 
         } catch (MalformedURLException ex) {
-            Logger.getLogger(TassUtilities.class.getName()).log(Level.SEVERE, null, ex);
+            Log.e("KESALAHAN", ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(TassUtilities.class.getName()).log(Level.SEVERE, null, ex);
+            Log.e("KESALAHAN", ex.getMessage());
         }
         return builder.toString();
     }
 
-//    public static void main(String[] args) {
-//        String nim = "6301114139";
-//        String pwd = "asaibae";
-//
-//        int rn = TassUtilities.randomGenerator();
-//        String api = "http://tass.telkomuniversity.ac.id/telkomuniversity.php/api?key=" + TassUtilities.md5(pwd) + "&nim=" + nim + "&type=dftap&a=" + rn + "&hsl=" + TassUtilities.generateToken(rn);
-//        System.out.println(api);
-//        System.out.println(TassUtilities.getPlainJSON(api));
-//    }
+    /**
+     * Fungsi untuk generate alamat API mentah
+     *
+     * @param nim      : nim mahasiswa di student portal
+     * @param password : password mahasiswa di student portal
+     * @param type     : tipe API yang akan dikeluarkan
+     * @param random   : angka random yang digenerate aplikasi
+     * @return hasil URL mentah dari API yang akan digunakan
+     */
+    private static String getTassApiUrl(String nim, String password, String type, int random) {
+        return BASE_API_URL + md5(password) + "&nim=" + nim + "&type=" + type + "&a=" + random + "&hsl=" + generateToken(random);
+    }
+
+    /**
+     * Fungsi API url generator, yang akan menghasilkan alamat url siap pakai
+     *
+     * @param nim      : nim mahasiswa di student portal
+     * @param password : password mahasiswa di student portal
+     * @param type     : tipe API yang akan dikeluarkan
+     * @return hasil akhir dari alamat API yang siap digunakan
+     */
+    public static String uriBuilder(String nim, String password, String type) {
+        int random = randomGenerator();
+
+        switch (type) {
+            case "login":
+                return getTassApiUrl(nim, password, type, random);
+            case "nm":
+                return getTassApiUrl(nim, password, type, random);
+            case "dftap":
+                return getTassApiUrl(nim, password, type, random);
+            default:
+                return getTassApiUrl(nim, password, type, random);
+        }
+    }
+
+    public static void main(String[] args) {
+        String nim = "6301114139";
+        String pwd = "asaibae";
+
+        System.out.println(TassUtilities.uriBuilder(nim, pwd, "login"));
+        System.out.println(TassUtilities.uriBuilder(nim, pwd, "nm"));
+        System.out.println(TassUtilities.uriBuilder(nim, pwd, "dftap"));
+    }
 }
