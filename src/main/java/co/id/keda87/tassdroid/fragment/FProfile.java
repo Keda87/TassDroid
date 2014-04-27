ba88dbd5dc1b79e7767504c0e96f6c05028d8676
Keda87 <keda_87@yahoo.com>
2014-04-27 21:05:41 +0700

    tambah lib v4, fragment 1%

---
+++ src/main/java/co/id/keda87/tassdroid/fragment/FProfile.java
@@ -0,0 +1,27 @@
+package co.id.keda87.tassdroid.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import co.id.keda87.tassdroid.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Keda87
+ * Date: 4/27/14
+ * Time: 7:25 PM
+ */
+public class FProfile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
