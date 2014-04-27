984e705c9ed621eea2141e57c7d7ec022e2c0042
Keda87 <keda_87@yahoo.com>
2014-04-27 13:20:41 +0700

    nav slider baru 1%

---
+++ src/main/java/co/id/keda87/tassdroid/pojos/SliderItem.java
@@ -0,0 +1,18 @@
+package co.id.keda87.tassdroid.pojos;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Keda87
+ * Date: 4/27/14
+ * Time: 1:18 PM
+ */
+public class SliderItem {
+
+    public String title;
+    public int icon;
+
+    public SliderItem(String title, int icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+}
