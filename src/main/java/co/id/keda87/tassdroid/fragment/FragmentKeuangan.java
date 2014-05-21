bf1855029cb6844e5a020024ad6b5605637a53cc
Keda87 <keda_87@yahoo.com>
2014-05-21 11:23:35 +0700

    tambah fitur swipe to refresh di fragment keuangan, tapi masih ada kekurangan handlingnya

--- src/main/java/co/id/keda87/tassdroid/fragment/FragmentKeuangan.java
+++ src/main/java/co/id/keda87/tassdroid/fragment/FragmentKeuangan.java
@@ -3,6 +3,7 @@ package co.id.keda87.tassdroid.fragment;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,38 +27,63 @@ import java.util.List;
  * Date: 5/5/14
  * Time: 11:12 AM
  */
-public class FragmentKeuangan extends Fragment {
+public class FragmentKeuangan extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private Gson gson;
     private SessionManager sessionManager;
     private ListView lvStatusKeuangan;
+    private SwipeRefreshLayout swipeRefresh;
+    private HashMap<String, String> userCredential;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_keuangan, container, false);
+
         //instance
         this.gson = new Gson();
         this.sessionManager = new SessionManager(getActivity());
+        this.userCredential = sessionManager.getUserDetails();
 
-        return inflater.inflate(R.layout.fragment_keuangan, null);
+        //instance widget
+        lvStatusKeuangan = (ListView) view.findViewById(R.id.lvKeuangan);
+        this.swipeRefresh = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
+
+        //set swipe listener
+        this.swipeRefresh.setOnRefreshListener(this);
+        this.swipeRefresh.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        //get user credential
-        HashMap<String, String> userCredential = sessionManager.getUserDetails();
-
         //start asynctask
-        new KeuanganTask().execute(
-                userCredential.get(SessionManager.KEY_USERNAME),
-                userCredential.get(SessionManager.KEY_PASSWORD)
-        );
+        if (TassUtilities.isConnected(getActivity())) {
+            new KeuanganTask().execute(
+                    this.userCredential.get(SessionManager.KEY_USERNAME),
+                    this.userCredential.get(SessionManager.KEY_PASSWORD)
+            );
+        } else {
+            TassUtilities.showToastMessage(getActivity(), R.string.login_page_alert_no_connection, 0);
+        }
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onRefresh() {
+        if (TassUtilities.isConnected(getActivity())) {
+            new KeuanganTask().execute(
+                    this.userCredential.get(SessionManager.KEY_USERNAME),
+                    this.userCredential.get(SessionManager.KEY_PASSWORD)
+            );
+        } else {
+            swipeRefresh.setRefreshing(false);
+            TassUtilities.showToastMessage(getActivity(), R.string.login_page_alert_no_connection, 0);
+        }
     }
 
     private class KeuanganTask extends AsyncTask<String, Void, List<StatusKeuangan>> {
@@ -78,8 +104,8 @@ public class FragmentKeuangan extends Fragment {
             super.onPostExecute(statusKeuangans);
 
             if (statusKeuangans != null) {
-                lvStatusKeuangan = (ListView) getActivity().findViewById(R.id.lvKeuangan);
                 lvStatusKeuangan.setAdapter(new KeuanganListAdapter(statusKeuangans, getActivity().getApplicationContext()));
+                swipeRefresh.setRefreshing(false);
                 Log.d("HASIL KEUANGAN", "Data ditampung di listview");
             } else {
                 Toast.makeText(getActivity(), "kesalahan", Toast.LENGTH_SHORT).show();
