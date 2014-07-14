c234f79644bad75b9e9fbdfcc28ef561d1b9b33d
Keda87 <keda_87@yahoo.com>
2014-07-15 00:44:23 +0700

    fitur perbarui biodata oke ;)

--- src/main/java/co/id/keda87/tassdroid/activities/ActivityBiodataEdit.java
+++ src/main/java/co/id/keda87/tassdroid/activities/ActivityBiodataEdit.java
@@ -1,12 +1,15 @@
 package co.id.keda87.tassdroid.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -32,19 +35,24 @@ public class ActivityBiodataEdit extends Activity {
 
     private SessionManager session;
     private HashMap<String, String> user;
+    private ProgressDialog dialog;
 
     @OnClick(R.id.bioEditButton)
     void aksiUpdateBiodata() {
-        Toast.makeText(this, "Klik bio!", Toast.LENGTH_LONG).show();
-        if (TassUtilities.isConnected(this)) {
-            new EditBioTask().execute(
-                    this.user.get(SessionManager.KEY_USERNAME),
-                    this.user.get(SessionManager.KEY_PASSWORD),
-                    this.telepon.getText().toString().trim(),
-                    this.jenisKelamin.getSelectedItemPosition() == 1 ? "L" : "P"
-            );
+        if (!this.telepon.getText().toString().trim().isEmpty() && this.jenisKelamin.getSelectedItemPosition() != 0) {
+            if (TassUtilities.isConnected(this)) {
+                new EditBioTask().execute(
+                        this.user.get(SessionManager.KEY_USERNAME),
+                        this.user.get(SessionManager.KEY_PASSWORD),
+                        this.telepon.getText().toString().trim(),
+                        this.jenisKelamin.getSelectedItemPosition() == 1 ? "L" : "P"
+                );
+            } else {
+                TassUtilities.showToastMessage(this, R.string.login_page_alert_no_connection, 0);
+            }
         } else {
-            TassUtilities.showToastMessage(this, R.string.login_page_alert_no_connection, 0);
+            Log.e("KESALAHAN", "Isi yang bener bro formnya");
+            return;
         }
     }
 
@@ -57,7 +65,10 @@ public class ActivityBiodataEdit extends Activity {
         //instance
         this.session = new SessionManager(this);
         this.user = this.session.getUserDetails();
+        this.dialog = new ProgressDialog(this);
 
+        this.dialog.setCancelable(false);
+        this.dialog.setMessage(getResources().getString(R.string.bio_edit_update_loading));
         this.telepon.setTypeface(TassUtilities.getFontFace(this, 0));
         this.headerLabel.setTypeface(TassUtilities.getFontFace(this, 1));
         this.btEditBio.setTypeface(TassUtilities.getFontFace(this, 0));
@@ -88,17 +99,33 @@ public class ActivityBiodataEdit extends Activity {
             //0 user, 1 pass, 2 telp, 3 jenisKelamin
             String bioUpdURL = TassUtilities.uriBuilder(params[0], params[1], "biodata", params[2], params[3]);
             Log.d("URL UPDATE BIO", bioUpdURL);
+            try {
+                TassUtilities.doGetJson(bioUpdURL);
+            } catch (Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e("KESALAHAN", "Error saat update biodata, ");
+                        TassUtilities.showToastMessage(ActivityBiodataEdit.this, R.string.bio_edit_gagal, 1);
+                        EditBioTask.this.cancel(true);
+                        dialog.dismiss();
+                    }
+                });
+            }
             return null;
         }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            dialog.show();
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+            dialog.dismiss();
+            TassUtilities.showToastMessage(ActivityBiodataEdit.this, R.string.bio_edit_berhasil, 1);
         }
     }
 }
